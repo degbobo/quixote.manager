2c1f0638902167e1fcc33c34d050795458d0efd6
Diego Urena <duren@cpp.edu>
2026-08-16 22:32:11 -0700

    Added functionality to enter data into quixote_logs

--- main.c
+++ main.c
@@ -3,3 +3,28 @@
 #include <methods.h>
 #include <direct.h>
 #include <windows.h>
+
+#include WRITE_KEYWORD "summon"
+
+#define FILE_PATH "C:\\Users\\duren\\projs\\Proj_Manager\\quixote_logs"
+
+// Writes to quixote_logs -> project_name;project_directory
+// entries sperated by a ';'
+void file_write(char * proj_name, char * directory) {
+	FILE * file;
+	file = GET_FILE(MODE_APPEND);
+
+	int index = 0;
+	while (proj_name[index] != '\0') {
+		fprintf(file, "%c", directory[index]);
+		index++;
+	}
+	fprintf(file, ";");
+	index = 0;
+	while (directory[index] != '\0') {
+		fprintf(file, "%c", directory[index]);
+		index++;
+	}
+	fprintf(file, "\n");
+	fclose(file);
+}
