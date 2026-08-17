3bbe9021f508f20935e49305951a65f0aa16a9e9
Diego Urena <duren@cpp.edu>
2026-08-16 21:09:24 -0700

    Created Header file for methods.c

---
+++ methods.h
@@ -0,0 +1,13 @@
+// needs <stdio.h>
+// needs <stdlib.h>
+
+int string_len(char * contents);
+int string_eqv(char * one, char * other);
+
+int fstring_len(FILE * position, char stopper);
+char* fstring_build(FILE * position, char stopper);
+void fstring_gone(char * string);
+
+FILE* GET_FILE(char mode);
+int FCURSOR_FIND(FILE * cursor, char hint);
+int FCURSOR_NEXTLINE(FILE * cursor);
